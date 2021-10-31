9f747a45a46fc23a240514e7246bb5e2f20369fa
takashi175 <91941767+takashi175@users.noreply.github.com>
2021-10-31 20:32:07 +0900

    Exam
    I started coding around 3:00 PM GMT+9
    I have added pagination, used beufy for adding components, and also implemented search filter for names only.

--- src/components/parts/UserSearchForm.vue
+++ src/components/parts/UserSearchForm.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="user-search-form">
     Here is UserSearchForm
+    <b-field label="Name" grouped horizontal>
+      <b-input placeholder="Search..." type="search" v-model="name"></b-input>
+      <p class="control">
+        <!-- <b-button class="button is-primary">Search</b-button> -->
+      </p>
+    </b-field>
+    <b-field label="Age" grouped horizontal>
+      <b-input placeholder="Search..." type="search"></b-input>
+      <p class="control">
+        <!-- <b-button class="button is-primary">Search</b-button> -->
+      </p>
+    </b-field>
+    <b-field label="Sex" grouped horizontal>
+      <b-input placeholder="Search..." type="search"></b-input>
+      <p class="control">
+        <b-button class="button is-primary" @click="onClickButton"
+          >Search</b-button
+        >
+      </p>
+    </b-field>
   </div>
 </template>
 
 <script>
-import Vue from "vue";
+//import Home from "../../views/Home.vue";
+export default {
+  name: "mainComponent",
+  data() {
+    return {
+      searchQuery: null,
+      resources: [],
+      name: "",
+      age: 0,
+      sex: false
+    };
+  },
 
-export default Vue.extend({});
+  methods: {
+    onClickButton() {
+      this.$emit("clicked", this.name);
+    }
+  }
+};
 </script>
 
 <style scoped>
 .user-search-form {
-  height: 60px;
+  height: 200px;
   background-color: yellow;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,67 @@
 <template>
   <div class="container my-5">
-    <user-search-form></user-search-form>
-    <b-table :data="users" :columns="columns"></b-table>
+    <user-search-form @clicked="queryEvent"></user-search-form>
+    <!-- <b-table :data="users" :columns="columns"></b-table> -->
+    <b-table
+      :data="filterMembers"
+      :paginated="isPaginated"
+      :per-page="perPage"
+      :current-page.sync="currentPage"
+      :pagination-simple="isPaginationSimple"
+      :pagination-position="paginationPosition"
+      :default-sort-direction="defaultSortDirection"
+      :pagination-rounded="isPaginationRounded"
+      :sort-icon="sortIcon"
+      :sort-icon-size="sortIconSize"
+      default-sort="name"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
+      aria-page-label="Page"
+      aria-current-label="Current page"
+    >
+      <b-table-column field="name" label="Name" sortable v-slot="props">
+        {{ props.row.name }}
+      </b-table-column>
+
+      <b-table-column field="age" label="Age" sortable v-slot="props">
+        {{ props.row.age }}
+      </b-table-column>
+
+      <b-table-column field="sex" label="Sex" sortable centered v-slot="props">
+        {{ props.row.sex }}
+      </b-table-column>
+      ></b-table
+    >
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import titanic from "@/assets/titanic.json";
+import titanic from "../assets/titanic.json";
 import UserSearchForm from "@/components/parts/UserSearchForm.vue";
+
+let userArray: any[] = [];
 
 export default Vue.extend({
   components: { UserSearchForm },
   name: "Home",
   data() {
     return {
+      searchValue: "",
+      isPaginated: true,
+      isPaginationSimple: false,
+      isPaginationRounded: false,
+      paginationPosition: "bottom",
+      defaultSortDirection: "asc",
+      sortIcon: "arrow-up",
+      sortIconSize: "is-small",
+      currentPage: 1,
+      perPage: 5,
+      prevIcon: "chevron-left",
+      nextIcon: "chevron-right",
       users: titanic,
+      buttonName: "",
+      searchQuery: null,
       columns: [
         {
           field: "name",
@@ -31,6 +77,27 @@ export default Vue.extend({
         }
       ]
     };
-  }
+  },
+
+  computed: {
+    filterMembers: function() {
+      let filtered = userArray;
+      if (this.searchValue) {
+        filtered = userArray.filter(
+          m => m.name.toLowerCase().indexOf(this.searchValue) > -1
+        );
+      }
+
+      return filtered;
+    }
+  },
+  methods: {
+    queryEvent: function(value: string) {
+      this.searchValue = value;
+    },
+  },
+  created() {
+    userArray = titanic;
+  },
 });
 </script>
